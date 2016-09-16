853e50ee7b9928636feafb41ab1183d6cf75ead4
ZengTianShengZ <zengtianshengz@163.com>
2016-09-16 12:28:12 +0800

    调试2

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,6 @@
 /**
  * Created by Administrator on 2016/9/13.
  */
-/*$(function(){
-    $('.owl-carousel').owlCarousel({
-        item:1,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:1000,
-        autoplayHoverPause:true
-    });
-});*/
 $(document).ready(function () {
 
-    function tesxxxx() {
-        $('.dasdsdsdsad').css('width',222);
-    }
 });
